de888b9b08e222d5d2b07bac899b2471d14ff041
u7149851 <u7149851@anu.edu.au>
2022-09-16 17:03:52 +0800

    首次提交

---
+++ src/test/java/com/nowcoder/community/LoggerTest.java
@@ -0,0 +1,29 @@
+package com.nowcoder.community;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes=CommunityApplication.class)
+public class LoggerTest {
+
+    //参数是类的名字，这样不同的logger就能知道是那个类的
+    private static final Logger logger = LoggerFactory.getLogger(LoggerTest.class);
+
+    @Test
+    public void testLogger(){
+        System.out.println(logger.getName());
+        logger.debug("debug log");
+        logger.info("info log");
+        logger.warn("warn log");
+        logger.error("error log");
+    }
+
+
+}
