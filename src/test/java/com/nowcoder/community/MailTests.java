66a82a32eccdc683db27535bf512937ae6d03d00
u7149851 <u7149851@anu.edu.au>
2022-12-01 03:16:43 +0800

    登录功能，退出功能, 显示登录信息

---
+++ src/test/java/com/nowcoder/community/MailTests.java
@@ -0,0 +1,38 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.util.MailClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes=CommunityApplication.class)
+public class MailTests {
+
+    @Autowired
+    private MailClient mailClient;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Test
+    public void textTestMail(){
+        mailClient.sendMail("xujuren@163.com", "Test", "Welcome.");
+    }
+
+    @Test
+    public void testHTML(){
+        Context context = new Context();
+        context.setVariable("username", "Sunday");
+
+        String content = templateEngine.process("/mail/demo", context);
+        System.out.println(content);
+        mailClient.sendMail("xujuren@163.com", "HTML", content);
+    }
+}
